7c12b494499c6cb79828c0fe6d5ca65dbe720d46
PMaciej155 <satan.fo@gmail.com>
2018-06-09 15:38:56 +0200

    added controller & view & thyme

---
+++ src/main/java/com/example/restaurantProj/Controller/LoginController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.restaurantProj.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author Maciej
+ */
+@Controller
+public class LoginController {
+    
+    @RequestMapping("/login")
+    public ModelAndView createUserView() {
+    ModelAndView mav = new ModelAndView();
+    mav.setViewName("login");
+    return mav;
+  }
+}
